261b225f100c8b0623f82e4438fe24a694c9d28e
Lucky-705 <99187514+Lucky-705@users.noreply.github.com>
2022-08-19 10:22:43 +0530

    Update eggcatcher.py
    commit

--- eggcatcher.py
+++ eggcatcher.py
@@ -2,7 +2,8 @@ from itertools import cycle
 from random import randrange
 from tkinter import Canvas, Tk, messagebox, font
 root = Tk()
-
+canvas_width=800
+canvas_height=800
 root.title("Egg Catcher")
 c = Canvas(root, width=canvas_width, height=canvas_height, background="deep sky blue")
 c.create_rectangle(-5, canvas_height-100, canvas_width+5, canvas_height+5, fill="sea green", width=0)
